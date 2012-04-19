b17b739e6d887ad923de669d0696beaa80a68041
ethan.le <ductoanle@gmail.com>
2012-04-19 14:51:54 +0800

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GoogleTVHackathon::Application.config.secret_token = '500a943b2d67b66f22cd96ad5f3a7ec5507baa9cd6fd72d9fb45d7e05ba90545bfefa19da98b8ce291da59a584e6e59f220997162f2ca8d215c3b848b9fdfd12'
